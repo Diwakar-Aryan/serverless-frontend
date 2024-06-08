0100ac9669da02bb6cdd767f08eb14502a28c9f3
sandeepkumarcu <sandeepkumarcu@hotmail.com>
2024-06-09 01:29:15 +0530

    add cart screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { WorkshopComponent } from './pages/workshop/workshop.component';
 import { CollectionsComponent } from './pages/collections/collections.component';
 import { JajamComponent } from './pages/jajam/jajam.component';
 import { NotesComponent } from './pages/notes/notes.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { ReturnPolicyComponent } from './pages/return-policy/return-policy.component';
 
 export const routes: Routes = [
     {
@@ -23,5 +26,15 @@ export const routes: Routes = [
     },
     {
         path: "notes", component: NotesComponent,
+    },
+    {
+        path: "cart", component: CartPageComponent,
+    },
+    {
+        path: "privacy", component: PrivacyPolicyComponent,
+    },
+    {
+        path: "returns", component: ReturnPolicyComponent,
     }
+
 ];
