13b970b3fd8d3fad0a6d5ad16243e41c59b1db1c
bibhanshu <bgupta535@hotmail.com>
2024-07-03 14:39:30 -0700

    images-section

--- src/app/pages/workshop/workshop-images-screen/natural-dye/natural-dye.component.ts
+++ src/app/pages/workshop/workshop-images-screen/natural-dye/natural-dye.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-natural-dye',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './natural-dye.component.html',
   styleUrl: './natural-dye.component.css'
 })
 export class NaturalDyeComponent {
+  isContentVisible = false;
+  isEightDayVisible = false;
+  isJaipur=false;
 
+  toggleContent() {
+    this.isContentVisible = !this.isContentVisible;
+  }
+  eightDayContent() {
+    this.isEightDayVisible = !this.isEightDayVisible;
+  }
+  jaipurContent() {
+    this.isJaipur = !this.isJaipur;
+  }
 }
--- src/app/pages/workshop/workshop-images-screen/resist-printing/resist-printing.component.ts
+++ src/app/pages/workshop/workshop-images-screen/resist-printing/resist-printing.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resist-printing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resist-printing.component.html',
   styleUrl: './resist-printing.component.css'
 })
 export class ResistPrintingComponent {
+  isContentVisible = false;
+  isEightDayVisible = false;
+  isJaipur=false;
 
+  toggleContent() {
+    this.isContentVisible = !this.isContentVisible;
+  }
+  eightDayContent() {
+    this.isEightDayVisible = !this.isEightDayVisible;
+  }
+  jaipurContent() {
+    this.isJaipur = !this.isJaipur;
+  }
 }
